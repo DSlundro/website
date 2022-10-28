4b46c0b17d955148d3c2bd8891895328f1c1e52e
Dragan <s.dragan.95@outlook.it>
2022-10-28 15:31:44 +0200

    add google map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,11 +1,25 @@
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 
-const Map = props => {
+const Map = () => {
+    const center = useMemo(() => ({lat: 45.94088, lng: 13.62167,}), [])
+    const {isLoaded} = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+    })
+    if(!isLoaded) return <div>Loading...</div>
 
     return (
-        <>
-
-        </>
-    )
+        <GoogleMap
+            zoom={15}
+            center={center}
+            mapContainerClassName='w-full h-[400px]'
+            mapTypeId="hybrid"
+        >
+            <Marker 
+                position={center}
+            />
+        </GoogleMap>
+    );
 }
-export default Map
+export default Map;
