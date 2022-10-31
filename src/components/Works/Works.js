a39bc4a742616f0d37776decb86bfc7b8081dd0c
Dragan <s.dragan.95@outlook.it>
2022-10-31 20:45:06 +0100

    add works title

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -8,21 +8,27 @@ import { worksList } from '../../Data/works';
 import { Section } from './../UI/Section';
 import { Container } from './../UI/Container';
 import { Row } from './../UI/Row';
+import { Title } from "../UI/Title";
+
 
 const Works = () => {
     const [works, setWorks] = useState([]);
+    const [currentWorks, setCurrentWorks] = useState([])
     const [currentPage, setCurrentPage] = useState(1);
     const [worksPerPage] = useState(9);
 
     useEffect(() => {
             setWorks(worksList);
-    }, [])
+    },[])
+
+    const totalWorks = works.length
     
     // Get current works
-    const indexOfLastWork = currentPage * worksPerPage;
-    const indexOfFirstPost = indexOfLastWork - worksPerPage;
-    const currentWorks = works.slice(indexOfFirstPost, indexOfLastWork)
-    const totalWorks = works.length
+    useEffect(() => {
+        const indexOfLastWork = currentPage * worksPerPage;
+        const indexOfFirstPost = indexOfLastWork - worksPerPage;
+        setCurrentWorks(works.slice(indexOfFirstPost, indexOfLastWork))
+    },[works, currentPage, worksPerPage])
 
     // Change page
     const paginate = (pageNumber) => {setCurrentPage(pageNumber)}
@@ -30,7 +36,11 @@ const Works = () => {
     return (
         <Section id='progetti'>
             <Container>
-                <Row className='md:!p-24 px-4 flex justify-center gap-4'>
+                <Row className='md:!p-24 px-4 flex !flex-col items-center justify-center gap-4 min-h-min'>
+                    <Title
+                        className='pb-7 !text-[46px] uppercase'
+                        text='Progetti'
+                    />
                     <WorkCard 
                         currentWorks={currentWorks}
                     />
