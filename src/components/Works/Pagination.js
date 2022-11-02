c52a0a65fc898e56e7df5f47ddebb8bdae8285f9
Dragan <s.dragan.95@outlook.it>
2022-11-02 17:35:46 +0100

    destructuring pagination

--- src/components/Works/Pagination.js
+++ src/components/Works/Pagination.js
@@ -1,20 +1,27 @@
 import classes from './scss/Pagination.module.scss'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 const Pagination = ({ worksPerPage, totalWorks, paginate }) => {
     const [isActive, setIsActive] = useState(0);
+    const [pages, setPages] = useState();
 
+    // Set pages
+    useEffect(() => {
+        setPages(Math.ceil(totalWorks / worksPerPage))
+    }, [totalWorks, worksPerPage, pages])
+
+    // Set active class
     const setActiveHandler = index => {
         if(isActive === index) return '!text-white !bg-black';
     }
     
-    const pages = Math.ceil(totalWorks / worksPerPage)
+    // Get numbers on pages
     const pageNumbers = []
-
     for(let i = 1; i <= pages; i++) {
         pageNumbers.push(i)
     }
+
 
     return (
         <nav className={classes.pag_nav}>
