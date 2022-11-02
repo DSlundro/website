a2bed9a42351d48041531a35a146bce5b4eb31b9
Dragan <s.dragan.95@outlook.it>
2022-11-02 12:56:43 +0100

    change api key

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,12 +1,11 @@
 import { useMemo } from "react";
 import { GoogleMap, useLoadScript, } from "@react-google-maps/api";
 
-const API_KEY = 'AIzaSyBn-q0VMtMOum5A7HVG86duHeJApbVDv7o'
 
 const Map = () => {
     const center = useMemo(() => ({lat: 45.94088, lng: 13.62167,}), [])
     const {isLoaded} = useLoadScript({
-        googleMapsApiKey: API_KEY
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
     })
     if(!isLoaded) return <div>Loading...</div>
 
